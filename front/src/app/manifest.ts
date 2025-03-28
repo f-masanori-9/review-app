784ec93a174e28c9c997eddf6dfe8c18fb715b2f
F-masanori <122769733+f-masanori-9@users.noreply.github.com>
2025-03-28 22:40:09 +0900

    change

--- front/src/app/manifest.ts
+++ front/src/app/manifest.ts
@@ -11,6 +11,11 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#000000",
     icons: [
       {
+        src: "/icon12_maskable.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
         src: "/icon512_maskable.png",
         sizes: "512x512",
         type: "image/png",
